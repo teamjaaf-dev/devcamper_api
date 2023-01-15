c45bc4cb1f9ec29b20a1b4fe5f00e9078d83f765
Md. Jafrin Hossain <md.jafrin.hossain@g.bracu.ac.bd>
2023-01-15 12:20:34 +0600

    router and controller done

--- server.js
+++ server.js
@@ -1,55 +1,16 @@
 const express = require('express')
-const dotenv = require('dotenv')
-const { restart } = require('nodemon')
+const dotenv = require('dotenv') 
 
+// Route files 
+const bootcamps = require('./routes/bootcamps')
 
 // load env vars
 dotenv.config({ path: './config/config.env'})
 
 const app = express()
 
-// routes 
-
-// get all bootcamps
-app.get('/api/v1/bootcamps', (req, res) => {
-    res.status(200).json({
-        success: true,
-        msg: "Show all bootcamp"
-    })
-})
-
-// get a single bootcamp
-app.get('/api/v1/bootcamps/:id', (req, res) => {
-    res.status(200).json({
-        success: true,
-        msg: `Show bootcamp ${req.params.id}`
-    })
-})
-
-// create new post
-app.post('/api/v1/bootcamps/:id', (req, res) => {
-    res.status(200).json({
-        success: true,
-        msg: "Create new bootcamp"
-    })
-})
-
-
-// update new bootcamp
-app.put('/api/v1/bootcamps/:id', (req, res) => {
-    res.status(200).json({
-        success: true,
-        msg: `Update bootcamp ${req.params.id}`
-    })
-})
-
-// delete a post
-app.delete('/api/v1/bootcamps/:id', (req, res) => {
-    res.status(200).json({
-        success: true,
-        msg: `Delete bootcamp ${req.params.id}`
-    })
-})
+// Moount Routers 
+app.use('/api/v1/bootcamps', bootcamps)
 
 const PORT = process.env.PORT || 5000
 
